91225f8cb25654389062c283315857326c187f85
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-05-19 05:14:50 -0300

    Don't even remember all the things, definitely took out ToggleTerm

--- init.lua
+++ init.lua
@@ -97,7 +97,7 @@ require('lazy').setup({
   'mrcjkb/haskell-tools.nvim',
 
   -- Terminal
-  { 'akinsho/toggleterm.nvim', version = "*", config = true },
+  -- { 'akinsho/toggleterm.nvim', version = "*", config = true },
 
   -- Move.nvim
   "fedepujol/move.nvim",
@@ -604,20 +604,20 @@ cmp.setup {
 -- )
 
 -- toggleterm
-require("toggleterm").setup()
-vim.keymap.set("n", ";tt", ":ToggleTerm<cr>")
-
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('n', '<esc>', ':ToggleTerm<cr>', opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
+-- require("toggleterm").setup()
+-- vim.keymap.set("n", ";tt", ":ToggleTerm<cr>")
+--
+-- function _G.set_terminal_keymaps()
+--   local opts = {buffer = 0}
+--   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+--   vim.keymap.set('n', '<esc>', ':ToggleTerm<cr>', opts)
+--   vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+--   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+-- end
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
@@ -636,8 +636,8 @@ local rt = require("rust-tools")
 rt.setup({
   server = {
     on_attach = function(_, bufnr)
-    vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-    vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<C-a>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
 })
