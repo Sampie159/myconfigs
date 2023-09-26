c34e2d0b66b892f16487ba6da5d3396517fe5fec
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-09-26 03:47:12 -0300

    bla

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln -sf ~/myconfigs/.zshrc ~/.zshrc
+ln -sf ~/myconfigs/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/myconfigs/.zshenv ~/.zshenv
